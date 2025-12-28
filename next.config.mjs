9bb7abbb80ac0bdf14350b2d0f5fd7b02bf07a67
Aseer Ruthbah <ruthbahbbc@gmail.com>
2025-12-28 14:28:38 +0600

    Add files via upload

---
+++ next.config.mjs
@@ -0,0 +1,12 @@
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    env: {
+        API_BASE_URL: process.env.REACT_APP_API_BASE_URL,
+    },
+};
+
+export default nextConfig;
